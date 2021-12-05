04d87011452c30f0b6632b70f281647408d7a334
AdityA <84802479+2802AdityA@users.noreply.github.com>
2021-12-05 06:45:14 +0530

    main

--- DI_UI.py
+++ DI_UI.py
@@ -1,14 +1,45 @@
-from PySide6.QtCore import (QCoreApplication, QEvent, QMetaObject, QSize, Qt)
+from PySide6 import QtWidgets
+from PySide6.QtCore import (QCoreApplication, QEvent, QMetaObject, QRect, QSize, Qt)
 from PySide6.QtGui import (QFont, QKeyEvent)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLineEdit,
-    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLineEdit, QMainWindow,QPushButton, QSizePolicy, QVBoxLayout, QWidget)
 import sympy as sym
+import sys
+
 x = sym.Symbol('x')
-style = ("QPushButton{\n"
+
+menu_button_style = ("QPushButton{\n"
+        "   background: transparent;\n"
+        "   font: 87 40pt \"Arial Black\";\n"
+        "   color: rgb(255, 255, 255);\n"
+        "   \n"
+        "   \n"
+        "   border-radius: 35px;\n"
+        "}\n"
+        "\n"
+        "QPushButton:hover{\n"
+        "    color: rgb(255, 255, 255);\n"
+        "    background: transparent;\n"
+        "    border: 5px solid white\n"
+        "}\n"
+        "\n"
+        "\n"
+        "\n"
+        "QPushButton:hover:!pressed\n"
+        "{\n"
+        "    background: transparent;\n"
+        "    color: rgb(255,255,255);\n"
+        "border: 0px solid white\n"
+        "}\n"
+        "\n"
+        "")
+menu_window_style = """background-image: url("wallpaper.jpg"); 
+        background-repeat: no-repeat; 
+        background-position: center;"""
+DnI_buttons_style = ("QPushButton{\n"
          "   font: 87 20pt \"Arial Black\";\n"
          "   color: rgb(255, 255, 255);\n"
          "   background-color: rgb(0, 0, 0);\n"
-         "   border-radius: 35-px;\n"
+         "   border-radius: 35px;\n"
          "}\n"
          "\n"
          "QPushButton:hover{\n"
@@ -29,10 +60,71 @@ style = ("QPushButton{\n"
          "\n"
          "")
 
-class Ui_MainWindow(QWidget):
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_Menu()
+        self.ui.setupUi(self)
+
+
+
+class Ui_Menu(QWidget):
     def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.MainWidget = QWidget(MainWindow)
+        self.MainWidget.setStyleSheet(menu_window_style)
+        self.VerticalLayout = QVBoxLayout(self.MainWidget)
+        
+        keys = [
+            ("Differentiation and Integration", Qt.Key_A, self.common),
+            ("Limits", Qt.Key_B, self.common),
+            ("Roots", Qt.Key_C, self.common),
+            ("History", Qt.Key_D, self.common),
+        ]
+        
+        for text, key, func in keys:
+            Button = QPushButton(self.MainWidget, text=text, focusPolicy=Qt.NoFocus)
+            Button.setProperty("__key__", key)
+            Button.setStyleSheet(menu_button_style)
+            
+            sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+            sizePolicy.setHorizontalStretch(0)
+            sizePolicy.setVerticalStretch(0)
+            sizePolicy.setHeightForWidth(Button.sizePolicy().hasHeightForWidth())
+            Button.setSizePolicy(sizePolicy)
+            
+            Button.clicked.connect(func)
+            self.VerticalLayout.addWidget(Button)
+            
+
+
+        MainWindow.setCentralWidget(self.MainWidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+    # retranslateUi
+    
+    def common(self):
+        main_ui.close()
+        
+        
+        
+        # app = QtWidgets.QApplication(sys.argv)
+        # main_window = QtWidgets.QMainWindow()
+        
+        dni_ui.show()
+        
+        
+        
+        
+
+class Ui_DnI(QWidget):
+    def setupUi(self, MainWindow):
         MainWindow.resize(800, 600)
         
     
@@ -78,47 +170,43 @@ class Ui_MainWindow(QWidget):
         
         self.GridLayout = QGridLayout(self.Keypadwidget)
         keys = [
-            ("Differentiate", Qt.Key_D, 0, 0, 1, 3),
-            ("Integrate", Qt.Key_I, 0, 3, 1, 3),
-            ("x", Qt.Key_X, 0, 6, 1, 1),
-            ("sin()", Qt.Key_S, 1, 0, 1, 1),
-            ("cos()", Qt.Key_C, 1, 1, 1, 1),
-            ("tan()", Qt.Key_T, 1, 2, 1, 1),
-            ("1", Qt.Key_1, 1, 3, 1, 1),
-            ("2", Qt.Key_2, 1, 4, 1, 1),
-            ("3", Qt.Key_3, 1, 5, 1, 1),
-            ("+", Qt.Key_Plus, 1, 6, 1, 1),
-            ("asin()", Qt.Key_L, 2, 0, 1, 1),
-            ("acos()", Qt.Key_M, 2, 1, 1, 1),
-            ("atan()", Qt.Key_N, 2, 2, 1, 1),
-            ("4", Qt.Key_4, 2, 3, 1, 1),
-            ("5", Qt.Key_5, 2, 4, 1, 1),
-            ("6", Qt.Key_6, 2, 5, 1, 1),
-            ("-", Qt.Key_Minus, 2, 6, 1, 1),
-            ("e", Qt.Key_E, 3, 0, 1, 1),
-            ("log()", Qt.Key_F, 3, 1, 1, 1),
-            ("x^y", Qt.Key_G, 3, 2, 1, 1),
-            ("7", Qt.Key_7, 3, 3, 1, 1),
-            ("8", Qt.Key_8, 3, 4, 1, 1),
-            ("9", Qt.Key_9, 3, 5, 1, 1),
-            ("×", Qt.Key_multiply, 3, 6, 1, 1),
-            ("Exit", Qt.Key_Exit, 4, 0, 1, 1),
-            ("(", Qt.Key_BracketRight, 4, 1, 1, 1),
-            (")", Qt.Key_BracketLeft, 4, 2, 1, 1),
-            ("C", Qt.Key_Clear, 4, 3, 1, 1),
-            ("0", Qt.Key_0, 4, 4, 1, 1),
-            ("⌫", Qt.Key_Backspace, 4, 5, 1, 1),
-            ("÷", Qt.Key_division, 4, 6, 1, 1),
+            ("Differentiate", Qt.Key_D, self.diff_clicked , 0, 0, 1, 3),
+            ("Integrate", Qt.Key_I, self.inte_clicked , 0, 3, 1, 3),
+            ("x", Qt.Key_X, self.on_clicked , 0, 6, 1, 1),
+            ("sin()", Qt.Key_S, self.on_clicked , 1, 0, 1, 1),
+            ("cos()", Qt.Key_C, self.on_clicked , 1, 1, 1, 1),
+            ("tan()", Qt.Key_T, self.on_clicked , 1, 2, 1, 1),
+            ("1", Qt.Key_1, self.on_clicked , 1, 3, 1, 1),
+            ("2", Qt.Key_2, self.on_clicked , 1, 4, 1, 1),
+            ("3", Qt.Key_3, self.on_clicked , 1, 5, 1, 1),
+            ("+", Qt.Key_Plus, self.on_clicked , 1, 6, 1, 1),
+            ("asin()", Qt.Key_L, self.on_clicked , 2, 0, 1, 1),
+            ("acos()", Qt.Key_M, self.on_clicked , 2, 1, 1, 1),
+            ("atan()", Qt.Key_N, self.on_clicked , 2, 2, 1, 1),
+            ("4", Qt.Key_4, self.on_clicked , 2, 3, 1, 1),
+            ("5", Qt.Key_5, self.on_clicked , 2, 4, 1, 1),
+            ("6", Qt.Key_6, self.on_clicked , 2, 5, 1, 1),
+            ("-", Qt.Key_Minus, self.on_clicked , 2, 6, 1, 1),
+            ("e", Qt.Key_E, self.on_clicked , 3, 0, 1, 1),
+            ("log()", Qt.Key_F, self.on_clicked , 3, 1, 1, 1),
+            ("x^y", Qt.Key_G, self.on_clicked , 3, 2, 1, 1),
+            ("7", Qt.Key_7, self.on_clicked , 3, 3, 1, 1),
+            ("8", Qt.Key_8, self.on_clicked , 3, 4, 1, 1),
+            ("9", Qt.Key_9, self.on_clicked , 3, 5, 1, 1),
+            ("×", Qt.Key_multiply, self.on_clicked , 3, 6, 1, 1),
+            ("Exit", Qt.Key_Exit, self.exit_clicked , 4, 0, 1, 1),
+            ("(", Qt.Key_BracketRight, self.on_clicked , 4, 1, 1, 1),
+            (")", Qt.Key_BracketLeft, self.on_clicked , 4, 2, 1, 1),
+            ("C", Qt.Key_Clear, self.clear_clicked , 4, 3, 1, 1),
+            ("0", Qt.Key_0, self.on_clicked , 4, 4, 1, 1),
+            ("⌫", Qt.Key_Backspace, self.backspace_clicked , 4, 5, 1, 1),
+            ("÷", Qt.Key_division, self.on_clicked , 4, 6, 1, 1),
         ]
         
-        for text, key, y, x, height, width in keys:
-            
-            TEXT = ["x", "sin()", "cos()", "tan()", "1", "2", "3", "+", "asin()", "acos()",
-                    "atan()", "x^y", "4", "5", "6", "-", "e", "log()", "7", "8", "9", "×", "(", ")", "0", "÷", "×"]
-            
+        for text, key, func, y, x, height, width in keys:
             Button = QPushButton(self.Keypadwidget, text=text, focusPolicy=Qt.NoFocus)
             Button.setProperty("__key__", key)
-            Button.setStyleSheet(style)
+            Button.setStyleSheet(DnI_buttons_style)
             
             sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Expanding)
             sizePolicy.setHorizontalStretch(0)
@@ -128,15 +216,8 @@ class Ui_MainWindow(QWidget):
             Button.setSizePolicy(sizePolicy)
             
             self.GridLayout.addWidget(Button, y, x, height, width)
-            if text in TEXT:
-                Button.clicked.connect(self.on_clicked)
-            elif text=="Differentiate":
-                Button.clicked.connect(self.diff_clicked)
-            elif text=="Integrate":
-                Button.clicked.connect(self.inte_clicked)
-            elif text=="C":
-                Button.clicked.connect(self.clear)
-
+            Button.clicked.connect(func)
+            
         self.VerticalLayout.addWidget(self.Keypadwidget)
 
         MainWindow.setCentralWidget(self.Mainwidget)
@@ -175,10 +256,34 @@ class Ui_MainWindow(QWidget):
         ans = str(sym.integrate(data, x))
         self.OutputlineEdit.setText(ans)
         
-    def clear(self):
+    def clear_clicked(self):
         self.InputlineEdit.setText("")
     
-    def backspace(self):
+    def backspace_clicked(self):
+        position = self.InputlineEdit.cursorPosition()
         data = self.InputlineEdit.text()
-        data = data[:-1]
+        data = data[:position-1] + data[position:]
         self.InputlineEdit.setText(data)
+        
+    def exit_clicked(self):
+        # ui.show()
+        main_ui.hide()
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+    # retranslateUi
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = QtWidgets.QMainWindow()
+    
+    
+    main_ui = MainWindow()
+    
+    dni_ui = Ui_DnI()
+    dni_ui.setupUi(main_window)
+    
+    main_ui.show()
+    app.exec()
+    del app
--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from DI_UI import Ui_MainWindow
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
 import sys
-
+# from try_1 import Ui_MainWindowww
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
+        # self.ui_2 = Ui_MainWindowww()
+        # self.ui_2.setupUi(self)
         self.ui.setupUi(self)
 import sys
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    
-    sys.exit(app.exec())
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+sys.exit(app.exec())
